59df66ac767a33af6c2d31a806bd63c2cab6f76e
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2023-01-20 21:58:11 +0100

    replaced the python handler with the go handler

--- projects/hello-world/_dist/lib/index.ts
+++ projects/hello-world/_dist/lib/index.ts
@@ -2,30 +2,28 @@ import { Duration } from "aws-cdk-lib";
 import { Rule, Schedule } from "aws-cdk-lib/aws-events";
 import { LambdaFunction as LambdaFunctionTarget } from "aws-cdk-lib/aws-events-targets";
 import {
-  InlineCode,
   Runtime,
   Function as LambdaFunction,
+  AssetCode,
+  Architecture,
 } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
-import { readFileSync } from "fs";
+import { join } from "path";
 
 export class LambdaCronService extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     const lambdaFn = new LambdaFunction(this, "LambdaFunction", {
-      code: new InlineCode(
-        readFileSync("lambda-handler.py", { encoding: "utf-8" })
-      ),
-      handler: "index.main",
+      code: new AssetCode(join(__dirname, "../../handler/lambda_deploy.zip")),
+      handler: "bootstrap",
       timeout: Duration.seconds(300),
-      runtime: Runtime.PYTHON_3_9,
+      runtime: Runtime.PROVIDED_AL2,
+      architecture: Architecture.ARM_64,
     });
 
-    // Run 6:00 PM UTC every Monday through Friday
-    // See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
     const rule = new Rule(this, "Rule", {
-      schedule: Schedule.expression("cron(0 18 ? * MON-FRI *)"),
+      schedule: Schedule.rate(Duration.minutes(1)),
     });
 
     rule.addTarget(new LambdaFunctionTarget(lambdaFn));
