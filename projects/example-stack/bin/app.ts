9b7bc0bafa6af40ec3129246e3229ec38003c5e0
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2023-01-25 15:19:36 +0100

    updated cache

--- projects/example-stack/bin/app.ts
+++ projects/example-stack/bin/app.ts
@@ -3,7 +3,7 @@ import { App, Stack } from "aws-cdk-lib";
 import { HelloWorldService } from "@bazel-poc/hello-world-service";
 
 const app = new App();
-const stack = new Stack(app, "ExampleStack");
+const stack = new Stack(app, "Stack");
 
 new HelloWorldService(stack, "HelloWorldService");
 
